dac175bcb7769d53019663ca330d00011e2b9e4c
Bohdan <duda.bohdan23@gmail.com>
2023-01-08 23:38:13 +0100

    uprava nacitani shop homepage a produz¨ct detail z db

--- BootShop/Controllers/Shop/HomeController.cs
+++ BootShop/Controllers/Shop/HomeController.cs
@@ -1,137 +1,18 @@
 ﻿using BootShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
 
 namespace BootShop.Controllers.Shop
 {
     public class HomeController : Controller
     {
+        private BootShopContext context = new BootShopContext();
+
         public IActionResult Index()
         {
-            Collection<ProductVariant> productVariants = new Collection<ProductVariant>();
-            productVariants.Add(new ProductVariant(
-                1,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                2,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                3,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                4,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                5,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                6,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                7,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-            productVariants.Add(new ProductVariant(
-                8,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                569,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png"));
-
-
-            ViewBag.ProductVariantCollection = productVariants;
-
+            ViewBag.ProductVariants = this.context.ProductVariants.OrderByDescending(v => v.Id).Take(8).Include(v => v.Product);
+            
             return View("/Views/Shop/Index.cshtml");
         }
     }
--- BootShop/Controllers/Shop/ProductDetailController.cs
+++ BootShop/Controllers/Shop/ProductDetailController.cs
@@ -1,26 +1,22 @@
 ﻿using BootShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BootShop.Controllers.Shop
 {
     public class ProductDetailController : Controller
     {
-        public IActionResult ProductDetail(string? productId = null)
+        private BootShopContext context = new BootShopContext();
+
+        public IActionResult ProductDetail(Product_Variant productVariant)
         {
-            ViewBag.ProductVariant = new ProductVariant(
-                1,
-                "Benjamin Velcro Dark Grey",
-                "Tyhle botky mají styl! Kožené barefoot tenisky vhodné na užší až normální nožku Vyrobené z kvalitní hladké kůže v kombinaci s textilem. Tenká podešev přesahuje přesprsty, aby byla botka chráněna před poškozením.",
-                579,
-                699,
-                true,
-                "Tmavě modrá",
-                "Do deště, turistické",
-                "Guma",
-                "Chlapecké",
-                "BG004-001",
-                "bota1.png",
-                "bota2.png");
+            ViewBag.ProductVariant = this.context.ProductVariants
+                .Include(v => v.Product)
+                .Include(v => v.Color)
+                .Include(v => v.Product.Brand)
+                .Include(v => v.Product.Category)
+                .Include(v => v.Product.Subcategory)
+                .FirstOrDefault(v => v.Id == productVariant.Id);
 
             return View("/Views/Shop/ProductDetail.cshtml");
         }
