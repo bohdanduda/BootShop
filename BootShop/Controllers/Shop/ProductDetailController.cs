fd891e1ad0a3c7c14d20fb141df11e8d89a1d383
Bohdan <duda.bohdan23@gmail.com>
2023-01-15 12:47:55 +0100

    vyber varianty produktu na detailu

--- BootShop/Controllers/Shop/ProductDetailController.cs
+++ BootShop/Controllers/Shop/ProductDetailController.cs
@@ -1,6 +1,7 @@
 ﻿using BootShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace BootShop.Controllers.Shop
 {
@@ -8,17 +9,67 @@ namespace BootShop.Controllers.Shop
     {
         private BootShopContext context = new BootShopContext();
 
-        public IActionResult ProductDetail(ProductVariant productVariant)
+        public IActionResult ProductDetail(ProductVariant inputProductVariant)
         {
-            ViewBag.ProductVariant = this.context.ProductVariants
-                .Include(v => v.Product)
-                .Include(v => v.Color)
-                .Include(v => v.Product.Brand)
-                .Include(v => v.Product.Category)
-                .Include(v => v.Product.Subcategory)
-                .FirstOrDefault(v => v.Id == productVariant.Id);
+            ProductVariant? productVariant = this.context.ProductVariants
+                .Include(pv => pv.Product)
+                .Include(pv => pv.Color)
+                .Include(pv => pv.Product.Brand)
+                .Include(pv => pv.Product.Category)
+                .Include(pv => pv.Product.Subcategory)
+                .FirstOrDefault(pv => pv.Id == inputProductVariant.Id);
+
+            if (productVariant == null)
+            {
+                throw new Exception("Varianta produktu neexistuje");
+            }
+
+            ViewBag.ProductVariant = productVariant;
+            ViewBag.DefaultProductVariantsForColors = this.GetDefaultVariantsForProductColors(productVariant.ProductId);
+            ViewBag.DefaultProductVariantsForSizes = this.GetDefaultVariantsForProductSizes(productVariant.ProductId);
 
             return View("/Views/Shop/ProductDetail.cshtml");
+        }
+
+        private List<ProductVariant> GetDefaultVariantsForProductColors(int productId)
+        {
+            List<ProductVariant> allProductVariants = this.context.ProductVariants
+                .Where(pv => pv.ProductId == productId)
+                .Include(pv => pv.Color)
+                .ToList();
+
+            List<Color> productColors = new List<Color>();
+            List<ProductVariant> defaultVariants = new List<ProductVariant>();
+            foreach (ProductVariant productVariant in allProductVariants)
+            {
+                if (!productColors.Contains(productVariant.Color))
+                {
+                    productColors.Add(productVariant.Color);
+                    defaultVariants.Add(productVariant);
+                }
+            }
+
+            return defaultVariants;
+        }
+
+        private List<ProductVariant> GetDefaultVariantsForProductSizes(int productId)
+        {
+            List<ProductVariant> allProductVariants = this.context.ProductVariants
+                .Where(pv => pv.ProductId == productId)
+                .ToList();
+
+            List<int> productSizes = new List<int>();
+            List <ProductVariant> defaultVariants = new List<ProductVariant>();
+            foreach (ProductVariant productVariant in allProductVariants)
+            {
+                if (!productSizes.Contains(productVariant.SizeId))
+                {
+                    productSizes.Add(productVariant.SizeId);
+                    defaultVariants.Add(productVariant);
+                }
+            }
+
+            return defaultVariants;
         }
     }
 }
